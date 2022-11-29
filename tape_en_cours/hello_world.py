ae5993baa8b1e1a46bce969695c79ae2c983609a
Matthieu Falce <matthieu@falce.net>
2022-11-29 16:00:19 +0100

    gitautosync update 21

--- tape_en_cours/hello_world.py
+++ tape_en_cours/hello_world.py
@@ -1,5 +1,7 @@
-def division():
-    res = i % 3 == 0
-
-
-division()
+for nombre in range(1, 101):
+    if nombre % 3 == 0:
+        print("fizz")
+    elif nombre % 5 == 0:
+        print("buzz")
+    elif nombre % 3 == 0 and nombre % 5 == 0:
+        print("fizzbuzz")
