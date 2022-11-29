775d684e36d244f135804290ba3f6452f99d59b7
Matthieu Falce <matthieu@falce.net>
2022-11-29 16:30:22 +0100

    gitautosync update 37

--- tape_en_cours/hello_world.py
+++ tape_en_cours/hello_world.py
@@ -8,6 +8,16 @@
 #     else:
 #         print(nombre)
 
+# for nombre in range(1, 101):
+#     res = ""
+#     if nombre % 3 == 0:
+#         res += "fizz"
+#     if nombre % 5 == 0:
+#         res += "buzz"
+#     if not res:
+#         res = str(nombre)
+#     print(res)
+
 for nombre in range(1, 101):
     res = ""
     if nombre % 3 == 0:
