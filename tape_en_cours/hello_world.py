3b6608569ce11bc18bcb075cd653695e4ccdaa6a
Matthieu Falce <matthieu@falce.net>
2022-11-30 09:55:27 +0100

    gitautosync update 115

--- tape_en_cours/hello_world.py
+++ tape_en_cours/hello_world.py
@@ -41,7 +41,7 @@ def regle_fizz_buzz_2(nombre: int) -> None:
     # return res
 
 
-print(list(map(regle_fizz_buzz_2, range(1, 101))))
+ma_liste = list(map(regle_fizz_buzz_2, range(1, 101)))
 
 # for nombre in range(1, 101):
 #     res = "fizz" * int(nombre % 3 == 0) + "buzz" * int(nombre % 5 == 0) or str(nombre)
